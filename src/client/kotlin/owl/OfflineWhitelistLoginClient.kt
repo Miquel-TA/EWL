581c1794e446e2ae72cc6cb43453a81791021b64
Miquel Trujillo Alvarez <miquel.trujillo@col.vueling.com>
2025-10-22 13:54:53 +0200

    Added Fabric 1.21.1 mod template

---
+++ src/client/kotlin/owl/OfflineWhitelistLoginClient.kt
@@ -0,0 +1,9 @@
+package owl
+
+import net.fabricmc.api.ClientModInitializer
+
+object OfflineWhitelistLoginClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
